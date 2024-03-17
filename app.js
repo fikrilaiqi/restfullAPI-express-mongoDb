71a78f77cd48fda87ff256e860dad801de7115e0
hamamullaiqi <hamamullaiqi@gmail.com>
2024-03-17 16:10:47 +0700

    setup environment variable

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env["PORT"];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,4 +14,4 @@ app.get("/", (req, res) => {
     res.end();
 });
 
-app.listen(PORT, () => `Server Running on PORT ${PORT}`);
+app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
