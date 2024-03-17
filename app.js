c6b12fcbbfced8459ee321148d735f9d0eff325d
hamamullaiqi <hamamullaiqi@gmail.com>
2024-03-17 16:04:56 +0700

    setup middlewares express

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
     res.send("Server Express");
     res.end();
